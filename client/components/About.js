7b0ce2d54c31b73b280274be0f65f0b97e71f006
Yasser Hussain <yasserhussain1110@gmail.com>
2017-01-27 13:08:43 +0530

    Added Readme. Updated Github link.

--- client/components/About.js
+++ client/components/About.js
@@ -4,6 +4,6 @@ export default () => (
   <footer>
     <span>React Redux Fullstack</span>|
     <a href="https://twitter.com/yasserhussain11">@yasserhussain</a>|
-    <a href="https://github.com/yasserhussain1110">Github</a>
+    <a href="https://github.com/yasserhussain1110/NightLifeApp">Github</a>
   </footer>
 );
