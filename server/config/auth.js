5960bd72dcd91677b34d6a3b1bf1db69e36b39d2
Yasser Hussain <yasserhussain1110@gmail.com>
2017-01-27 12:54:28 +0530

    Twitter callback for production

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,8 +1,11 @@
 // expose our config directly to our application using module.exports
+
 module.exports = {
   'twitterAuth': {
     'consumerKey': 'jqtCewu5Ys03Nq8jB8M8ZGrfb',
     'consumerSecret': 'lJAhyC3GxL61gn4Yd4eZt7eMJ50Qt0SZ97Xw2UZE4pqTX6CVLS',
-    'callbackURL': 'http://localhost:8080/auth/twitter/callback'
+    'callbackURL':  process.env.NODE_ENV === "production" ?
+      'https://yasser-nightlife-app.herokuapp.com/auth/twitter/callback' :
+      'http://localhost:8080/auth/twitter/callback'
   }
 };
